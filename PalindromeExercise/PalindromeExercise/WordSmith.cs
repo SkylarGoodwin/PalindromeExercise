775e7cb43d45bfe59d30cc8dc7f25dd0a6e3c1c1
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-04-11 21:37:17 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+
+    {
+        public bool IsAPalindrome(string word)
+        {
+            char[] reverse = word.ToCharArray();
+            Array.Reverse(reverse);
+            string x = new string(reverse);
+
+            if (word == x)
+            {  return true; }
+            else { return false; }
+        }
+    }
+}
